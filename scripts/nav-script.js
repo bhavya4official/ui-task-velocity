ec7ff11b16a3852f248949f28b7ed332d4663ce4
Bhavya SIngh <bhavya4official@gmail.com>
2021-06-07 20:11:34 +0530

    Commit

---
+++ scripts/nav-script.js
@@ -0,0 +1,47 @@
+// var mybutton = document.getElementById("myBtn");
+// var header = document.getElementById("navbar");
+// var navButton = document.getElementById("nav");
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//     header.style.background = "#FFF";
+//     // navButton.style.color = "#000";
+//     navButton.getElementsByClassName("navfield")[0].style.color = "#000";
+//     navButton.getElementsByClassName("navfield")[1].style.color = "#000";
+//     navButton.getElementsByClassName("navfield")[2].style.color = "#000";
+//     navButton.getElementsByClassName("navfield")[3].style.color = "#000";
+//     navButton.getElementsByClassName("navfield")[4].style.color = "#000";
+//   }
+//   else {
+//     mybutton.style.display = "none";
+//     header.style.background = "#FFF5";
+
+//   }
+//   //   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+//   //     //   alert("ok")
+//   //     const element = document.querySelector('.box_clsss4');
+//   //     element.classList.add('animate__animated', 'animate__bounceOutLeft');
+//   // }        
+//   // When the user clicks on the button, scroll to the top of the document
+//   function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+//   }
+//   function clickSearch() {
+//     document.getElementById("searchBox").style.display = "inline";
+//     document.getElementById("search-icon").style.display = "none";
+//   }
+
+//   // window.onload = function(){
+//   //     var searchBox = document.getElementById('searchBox');
+//   //     document.onclick = function(e){
+//   //        if(e.target.id !== 'searchBox'){
+//   //           searchBox.style.display = 'none';
+//   //           document.getElementById("search-icon").style.display = "inline";
+//   //        }
+//   //     }
+
+// }
